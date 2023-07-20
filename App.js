c4f993b79224fdb7de36b0958c7a6698d9e9b79c
JustVibhor <vibhorebhatneriya@gmail.com>
2023-07-21 03:28:05 +0530

    sidebar confusion

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
-import { StatusBar } from "expo-status-bar";
-import { Button, ImageBackground, StyleSheet, Text, View } from "react-native";
-import { CATEGORIES } from "./data/dummy-data";
+import { useState } from "react";
+import { ImageBackground, StyleSheet, Text, View } from "react-native";
 import HomePage from "./screens/HomePage";
-import ChoiceButton from "./ui/ChoiceButton";
-import Title from "./ui/Title";
 
 export default function App() {
+  const [open, setOpen] = useState(false);
+
   return (
     <ImageBackground
       source={require("./assets/images/coffee.jpg")}
@@ -21,10 +20,6 @@ export default function App() {
 const styles = StyleSheet.create({
   rootContainer: {
     flex: 1,
-  },
-  homeContainer: {
-    marginVertical: 40,
-    marginHorizontal: 20,
   },
   backgroundImage: {
     opacity: 0.19,
