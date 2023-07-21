2090f21eff100f571a093a7b2eef00a795ce5c56
JustVibhor <vibhorebhatneriya@gmail.com>
2023-07-21 15:37:07 +0530

    adding ripple effect

--- ui/ChoiceButton.js
+++ ui/ChoiceButton.js
@@ -3,9 +3,9 @@ import { Pressable, StyleSheet, Text, View } from "react-native";
 function ChoiceButton({ title, color }) {
   return (
     <View style={styles.buttonContainer}>
-      <Pressable>
+      <Pressable android_ripple={{ color: "#FFA41B" }} style={styles.button}>
         <View style={styles.innerContainer}>
-          <Text>{title}</Text>
+          <Text style={styles.buttonText}>{title}</Text>
         </View>
       </Pressable>
     </View>
@@ -16,10 +16,18 @@ const styles = StyleSheet.create({
   buttonContainer: {
     flex: 1,
     height: 150,
-    backgroundColor: "#F4D160",
+    backgroundColor: "#FFD9C0",
     borderRadius: 8,
     margin: 15,
     elevation: 4,
+    shadowColor: "black",
+    shadowOpacity: 0.25,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 8,
+    overflow: "hidden",
+  },
+  button: {
+    flex: 1,
   },
   innerContainer: {
     flex: 1,
@@ -29,7 +37,8 @@ const styles = StyleSheet.create({
   },
   buttonText: {
     color: "white",
-    fontSize: 20,
+    fontWeight: "900",
+    fontSize: 18,
     fontWeight: 900,
   },
 });
