d06b92aa3a7cdefb6192d08a58023cc5d38ee3e9
JustVibhor <vibhorebhatneriya@gmail.com>
2023-07-24 16:55:31 +0530

    navigator, {} not really working

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,10 +1,23 @@
+import { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { MEALS } from "../data/dummy-data";
 
 function MealsOverviewScreen({ route }) {
+  const { meal, setMeal } = useState();
+
   const { itemId } = route.params;
+
+  function handleItem() {
+    setMeal(() => {
+      return MEALS.find((meal) => meal.id);
+    });
+  }
+
   return (
     <View>
-      <Text>Item: {JSON.stringify(itemId)}</Text>
+      <Text>
+        Item: {JSON.stringify(handleItem)} {meal}
+      </Text>
     </View>
   );
 }
