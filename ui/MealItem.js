fe303e91ddeb0a948cddc5be48166f907e1b02af
JustVibhor <vibhorebhatneriya@gmail.com>
2023-07-25 23:17:46 +0530

    improving MealItem design

--- ui/MealItem.js
+++ ui/MealItem.js
@@ -1,4 +1,11 @@
-import { Image, Pressable, StyleSheet, Text, View } from "react-native";
+import {
+  Image,
+  Platform,
+  Pressable,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 
 function MealItem({ item }) {
   return (
@@ -9,9 +16,9 @@ function MealItem({ item }) {
           <Text style={styles.title}>{item.title}</Text>
         </View>
         <View style={styles.desc}>
-          <Text>Duration: {item.duration}</Text>
-          <Text>Affordability: {item.affordability}</Text>
-          <Text>complexity: {item.complexity}</Text>
+          <Text style={styles.detailItem}>{item.duration}</Text>
+          <Text style={styles.detailItem}>{item.affordability}</Text>
+          <Text style={styles.detailItem}>{item.complexity}</Text>
         </View>
       </Pressable>
     </View>
@@ -22,10 +29,13 @@ const styles = StyleSheet.create({
   mealItem: {
     margin: 16,
     borderRadius: 8,
-    overflow: "hidden",
+    overflow: Platform.OS === "android" ? "hidden" : "visible",
     backgroundColor: "white",
     elevation: 4,
     shadowColor: "black",
+    shadowOpacity: 0.25,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 3,
   },
 
   image: {
@@ -37,10 +47,20 @@ const styles = StyleSheet.create({
     fontSize: 18,
     fontWeight: "bold",
     textAlign: "center",
+    margin: 8,
+    fontWeight: "bold",
   },
 
   desc: {
-    // backgroundColor: "#FFF38C",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 8,
+  },
+  detailItem: {
+    fontSize: 14,
+    marginHorizontal: 4,
+    textAlign: "center",
   },
 });
 
